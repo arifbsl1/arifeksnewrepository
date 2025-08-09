9aa532a189e3957c9f78519814aedb33e8210c7d
arif <arif.bhuswal@gmail.com>
2025-08-09 20:42:05 +0530

    Changes done for auth

--- orderbackend/src/main/java/com/example/orderbackend/Application.java
+++ orderbackend/src/main/java/com/example/orderbackend/Application.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.example.orderbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- productbackend/src/main/java/com/example/productbackend/Application.java
+++ productbackend/src/main/java/com/example/productbackend/Application.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.example.productbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
